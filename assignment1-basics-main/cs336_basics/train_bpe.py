5d0be58d3fca252be593c3ad5e77c819854403dc
Mingfei Li <ml@mingfei.io>
2025-07-26 01:44:14 -0700

    BPE: optimize training speed (round 1)

--- assignment1-basics-main/cs336_basics/train_bpe.py
+++ assignment1-basics-main/cs336_basics/train_bpe.py
@@ -2,6 +2,7 @@ import time
 import os
 import regex as re
 from collections import Counter
+from collections import defaultdict
 from functools import partial
 from multiprocessing import Pool
 from typing import BinaryIO
@@ -86,17 +87,18 @@ def pretokenize_chunk(
     return pretoken_counts
 
 def count_token_pairs(
-    pretoken_count: tuple[tuple[bytes], int],
-) -> Counter:
-    pretoken, multiplier = pretoken_count
-    pair_counts = Counter(zip(pretoken[:-1], pretoken[1:]))
-    return Counter({pair: count * multiplier for pair, count in pair_counts.items()})
+    pretoken_counts: tuple[bytes],
+) -> dict[tuple[bytes, bytes], int]:
+    pretoken, multiplier = pretoken_counts
+    pair_counts = defaultdict(int)
+    for token_1, token_2 in zip(pretoken[:-1], pretoken[1:]):
+       pair_counts[(token_1, token_2)] += multiplier
+    return pair_counts
 
 def apply_merge(
     merge: tuple[bytes, bytes],
-    pretoken_count,
+    pretoken,
 ) -> Counter[tuple[bytes]]:
-    pretoken, count = pretoken_count
     new_pretoken = []
     i = 0
     merged_token = merge[0] + merge[1]
@@ -108,7 +110,7 @@ def apply_merge(
         else:
             new_pretoken.append(pretoken[i])
             i += 1
-    return Counter({tuple(new_pretoken): count})
+    return tuple(new_pretoken)
 
 def train_bpe(
     input_path: str | os.PathLike,
@@ -120,31 +122,36 @@ def train_bpe(
         boundaries = find_chunk_boundaries(
             input_path, 1_000_000, "<|endoftext|>".encode("utf-8"))
 
-        pretoken_counts = sum(p.map(
+        pretoken_count_dict = sum(p.map(
             partial(pretokenize_chunk, input_path, special_tokens),
             list(zip(boundaries[:-1], boundaries[1:])),
         ), Counter())
+        pretokens = pretoken_count_dict.keys()
+        pretoken_counts = pretoken_count_dict.values()
 
         token_set = (
-            set(token for pretoken in pretoken_counts for token in pretoken)
+            set(token for pretoken in pretokens for token in pretoken)
             | set(token.encode("utf-8") for token in special_tokens + ["<|endoftext|>"])
         )
         merges = []
 
         while len(token_set) < vocab_size:
-            pair_counts = sum(p.map(
+            pair_counts_per_pretoken = p.map(
                 count_token_pairs,
-                list(pretoken_counts.items()),
-            ), Counter())
+                list(zip(pretokens, pretoken_counts)),
+            )
+            total_pair_counts = defaultdict(int)
+            for pair_counts in pair_counts_per_pretoken:
+                for pair, count in pair_counts.items():
+                    total_pair_counts[pair] += count
 
-            merge, _ = max(pair_counts.items(), key=lambda x: (x[1], x[0]))
+            merge, _ = max(total_pair_counts.items(), key=lambda x: (x[1], x[0]))
             merges.append(merge)
             token_set.add(merge[0]+merge[1])
-
-            pretoken_counts = sum(p.map(
+            pretokens = list(p.map(
                 partial(apply_merge, merge),
-                list(pretoken_counts.items()),
-            ), Counter())
+                pretokens,
+            ))
 
         vocab = {index: token for index, token in enumerate(token_set)}
         return vocab, merges
@@ -153,11 +160,9 @@ if __name__ == "__main__":
     print(os.getcwd())
     start_time = time.time()
     vocab, merges = train_bpe(
-        "assignment1-basics-main/data/test.txt",
-        100,
+        "data/TinyStoriesV2-GPT4-valid.txt",
+        10000,
         ["<|endoftext|>"],
     )
-    str_vocab = [token.decode("utf-8", errors="ignore") for token in vocab.values()]
-    print(str_vocab)
     end_time = time.time()
     print(f"Total time: {end_time - start_time} seconds")
