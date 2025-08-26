c753cae15d2de018babacc6395023ba1676dfe2c
Mingfei Li <ml@mingfei.io>
2025-08-25 23:01:43 -0700

    Training utils: AdamW

--- assignment1-basics-main/cs336_basics/nn_utils.py
+++ assignment1-basics-main/cs336_basics/nn_utils.py
@@ -1,4 +1,5 @@
 import torch
+from collections.abc import Callable, Iterator
 
 def cross_entropy(logits: torch.Tensor, targets: torch.Tensor):
     max_logits, _ = logits.max(dim=-1, keepdim=True)
@@ -6,3 +7,43 @@ def cross_entropy(logits: torch.Tensor, targets: torch.Tensor):
     y = torch.gather(logits, dim=1, index=targets.unsqueeze(-1))
     z = torch.log(torch.exp(logits).sum(dim=-1, keepdim=True))
     return (z-y).mean()
+
+class AdamW(torch.optim.Optimizer):
+    def __init__(
+        self,
+        params: Iterator[torch.nn.Parameter],
+        lr: float,
+        weight_decay: float,
+        betas: tuple[float, float],
+        eps=1e-8,
+    ) -> None:
+        defaults = {"lr": lr}
+        super().__init__(params, defaults)
+        self.betas = betas
+        self.weight_decay = weight_decay
+        self.eps = eps
+
+    def step(self, closure: Callable | None = None):
+        loss = None if closure is None else closure()
+        for group in self.param_groups:
+            lr = group["lr"]
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+
+                state = self.state[p]
+                t = state.get("t", 0)
+                m = state.get("m", torch.zeros_like(p.grad))
+                v = state.get("v", torch.zeros_like(p.grad))
+
+                t = t + 1
+                m = self.betas[0] * m + (1-self.betas[0]) * p.grad
+                v = self.betas[1] * v + (1-self.betas[1]) * (p.grad**2)
+
+                alpha_t = lr * (1 - self.betas[1]**t)**0.5 / (1 - self.betas[0]**t)
+                p.data -= alpha_t * m / (v + self.eps)**0.5
+                p.data -= lr * self.weight_decay * p.data
+                state["t"] = t
+                state["m"] = m
+                state["v"] = v
+        return loss
--- assignment1-basics-main/tests/adapters.py
+++ assignment1-basics-main/tests/adapters.py
@@ -25,6 +25,7 @@ from cs336_basics.model import (
 )
 from cs336_basics.nn_utils import (
     cross_entropy,
+    AdamW,
 )
 
 def run_linear(
@@ -525,7 +526,7 @@ def get_adamw_cls() -> type[torch.optim.Optimizer]:
     """
     Returns a torch.optim.Optimizer that implements AdamW.
     """
-    raise NotImplementedError
+    return AdamW
 
 
 def run_get_lr_cosine_schedule(
