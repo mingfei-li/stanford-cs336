c68c13785caa4c233acbbc22463c7e1d3e74fc24
Mingfei Li <ml@mingfei.io>
2025-08-28 22:36:25 -0700

    Training utils: learning_rate_schedule

--- assignment1-basics-main/cs336_basics/nn_utils.py
+++ assignment1-basics-main/cs336_basics/nn_utils.py
@@ -1,3 +1,4 @@
+import math
 import torch
 from collections.abc import Callable, Iterator
 
@@ -47,3 +48,21 @@ class AdamW(torch.optim.Optimizer):
                 state["m"] = m
                 state["v"] = v
         return loss
+
+def get_lr_cosine_schedule(
+    it: int,
+    max_learning_rate: float,
+    min_learning_rate: float,
+    warmup_iters: int,
+    cosine_cycle_iters: int,
+) -> float:
+    if it < warmup_iters:
+        return (it / warmup_iters) * max_learning_rate
+    elif it <= cosine_cycle_iters:
+        return (
+            min_learning_rate + 
+            ((1 + math.cos((it - warmup_iters) / (cosine_cycle_iters - warmup_iters) * math.pi)) / 2) *
+            (max_learning_rate - min_learning_rate)
+        )
+    else:
+        return min_learning_rate
