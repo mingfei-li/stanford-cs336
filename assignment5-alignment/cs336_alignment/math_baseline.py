0aea293ed65cf7109b74ef35d8a9d12aa6e2de50
Mingfei Li <ml@mingfei.io>
2025-10-28 08:03:17 +0000

    Alignment: math_baseline (code complete)

--- assignment5-alignment/cs336_alignment/math_baseline.py
+++ assignment5-alignment/cs336_alignment/math_baseline.py
@@ -1,11 +1,11 @@
 from typing import Callable
 from collections import defaultdict
+import json
 
 from vllm import LLM, SamplingParams
-import pandas as pd
-import pyarrow.parquet as pq
 
 from cs336_alignment.drgrpo_grader import r1_zero_reward_fn
+from cs336_alignment.datasets import GSM8KTestDataset
 
 def evaluate_vllm(
     vllm_model: LLM,
@@ -14,23 +14,34 @@ def evaluate_vllm(
     ground_truths: list[str],
     eval_sampling_params: SamplingParams,
 ) -> None:
-    outputs = vllm_model.generate(prompts, eval_sampling_params)
+    """
+    Evaluate a language model on a list of prompts,
+    compute evaluation metrics, and serialize results to disk.
+    """
+
     total_rewards = defaultdict(float)
+    samples = []
+    outputs = vllm_model.generate(prompts, eval_sampling_params)
     for output, ground_truth in zip(outputs, ground_truths):
-        prompt = output.prompt
         generated_text = output.outputs[0].text
+        
+        sample = {
+            "prompt": output.prompt,
+            "generated_text": generated_text,
+        }
         rewards = reward_fn(generated_text, ground_truth)
         for k, v in rewards.items():
+            sample[k] = v
             total_rewards[k] += v
-    print(total_rewards)
+        samples.append(sample)
+    with open("data/math_baseline.json", "w") as f:
+        json.dump(samples, f, indent=4)
+    print(f"=== Total rewards for {len(prompts)} samples ===")
+    print(f" - format_reward: {total_rewards["format_reward"]}")
+    print(f" - answer_reward: {total_rewards["answer_reward"]}")
+    print(f" - reward: {total_rewards["reward"]}")
 
 if __name__ == "__main__":
-    with open("cs336_alignment/prompts/r1_zero.prompt", "r") as f:
-        template = f.read()
-    df = pd.read_parquet("data/gsm8k_main_test.parquet", engine="pyarrow")
-    prompts = [template.replace("{question}", question) for question in df["question"]]
-    ground_truths = df["answer"]
-
     sampling_params = SamplingParams(
         temperature=1.0,
         top_p=1.0,
@@ -39,10 +50,11 @@ if __name__ == "__main__":
         include_stop_str_in_output=True,
     )
     llm = LLM(model="Qwen/Qwen2.5-Math-1.5B")
+    ds = GSM8KTestDataset()
     evaluate_vllm(
         vllm_model=llm,
         reward_fn=r1_zero_reward_fn,
-        prompts=prompts,
-        ground_truths=ground_truths,
+        prompts=ds.prompts,
+        ground_truths=ds.ground_truths,
         eval_sampling_params=sampling_params,
     )
