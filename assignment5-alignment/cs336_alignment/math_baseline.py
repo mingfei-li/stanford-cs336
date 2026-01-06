090967783feba8cb0771b14e6f913dc6a6da4d52
Mingfei Li <limingfei@gmail.com>
2026-01-06 14:02:55 -0800

    Reset assignment 5

--- assignment5-alignment/cs336_alignment/math_baseline.py
+++ assignment5-alignment/cs336_alignment/math_baseline.py
@@ -1,61 +1,8 @@
-from typing import Callable
-from collections import defaultdict
-import json
-
 from vllm import LLM, SamplingParams
-
-from cs336_alignment.drgrpo_grader import r1_zero_reward_fn
-from cs336_alignment.datasets import GSM8KTestDataset
 
 def evaluate_vllm(
     vllm_model: LLM,
     reward_fn: Callable[[str, str], dict[str, float]],
-    prompts: list[str],
-    ground_truths: list[str],
+    prompts: List[str],
     eval_sampling_params: SamplingParams,
 ) -> None:
-    """
-    Evaluate a language model on a list of prompts,
-    compute evaluation metrics, and serialize results to disk.
-    """
-
-    total_rewards = defaultdict(float)
-    samples = []
-    outputs = vllm_model.generate(prompts, eval_sampling_params)
-    for output, ground_truth in zip(outputs, ground_truths):
-        generated_text = output.outputs[0].text
-        
-        sample = {
-            "prompt": output.prompt,
-            "generated_text": generated_text,
-            "ground_truth": ground_truth,
-        }
-        rewards = reward_fn(generated_text, ground_truth)
-        for k, v in rewards.items():
-            sample[k] = v
-            total_rewards[k] += v
-        samples.append(sample)
-    with open("data/math_baseline.json", "w") as f:
-        json.dump(samples, f, indent=4)
-    print(f"=== Total rewards for {len(prompts)} samples ===")
-    print(f" - format_reward: {total_rewards['format_reward']}")
-    print(f" - answer_reward: {total_rewards['answer_reward']}")
-    print(f" - reward: {total_rewards['reward']}")
-
-if __name__ == "__main__":
-    sampling_params = SamplingParams(
-        temperature=1.0,
-        top_p=1.0,
-        max_tokens=1024,
-        stop=["</answer>"],
-        include_stop_str_in_output=True,
-    )
-    llm = LLM(model="Qwen/Qwen2.5-Math-1.5B")
-    ds = GSM8KTestDataset()
-    evaluate_vllm(
-        vllm_model=llm,
-        reward_fn=r1_zero_reward_fn,
-        prompts=ds.prompts,
-        ground_truths=ds.ground_truths,
-        eval_sampling_params=sampling_params,
-    )
--- assignment5-alignment/tests/adapters.py
+++ assignment5-alignment/tests/adapters.py
@@ -8,7 +8,6 @@ from torch import Tensor
 from torch.utils.data import Dataset
 from transformers import PreTrainedTokenizerBase
 
-from cs336_alignment.sft import tokenize_prompt_and_output
 
 def run_tokenize_prompt_and_output(
     prompt_strs: list[str],
@@ -32,7 +31,7 @@ def run_tokenize_prompt_and_output(
             "response_mask": torch.Tensor of shape (batch_size, max(prompt_and_output_lens) - 1):
                 a mask on the response tokens in `labels`.
     """
-    return tokenize_prompt_and_output(prompt_strs, output_strs, tokenizer)
+    raise NotImplementedError
 
 
 def run_compute_group_normalized_rewards(
